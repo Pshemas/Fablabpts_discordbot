756159f46ccde295579299e35a68af1cf08cac06
Przemek Jeske <jeskenis@gmail.com>
2021-08-09 18:29:06 +0200

    started error handling

---
+++ errors.py
@@ -0,0 +1,7 @@
+import discord
+
+class CommandError(Exception):
+    def __init__(self, message):
+        self.message = message
+        await message.channel.send("Twój FabcoinBot nie wie co zrobić. Wpisz $pomoc żeby zobaczyć poprawną składnię dostępnych poleceń %s" % message)
+        super().__init__(message)
