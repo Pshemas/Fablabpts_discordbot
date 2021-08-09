184d29d84d7439e417839b7edeec8c48c63496f8
Przemek Jeske <jeskenis@gmail.com>
2021-08-09 19:35:08 +0200

    reverting error handling. Need to figure out howto do it properly.

--- errors.py
+++ errors.py
@@ -1,7 +1,1 @@
-import discord
-
-class CommandError(Exception):
-    def __init__(self, message):
-        self.message = message
-        message.channel.send("Twój FabcoinBot nie wie co zrobić. Wpisz $pomoc żeby zobaczyć poprawną składnię dostępnych poleceń %s" % message)
-        super().__init__(message)
+#Hopefully will figure out howto implement those
--- messageops.py
+++ messageops.py
@@ -1,5 +1,5 @@
 from errors import *
-
+import discord
 
 def extractdata(message):
     ''' extracts data for Google Sheets from message, 
@@ -7,17 +7,11 @@ def extractdata(message):
 
     withoutprefix = message.clean_content
     cleaned = withoutprefix.split(' ', 2)
-    if len(cleaned) != 2:
-        raise CommandError("Brak informacji za co te fabcoiny :( .")
 
-    else:
-        try:
-            cleaned[1] = int(cleaned[1])
-            dataset = {"discordid": message.author.id, "name": message.author.name + '#' +
-                       message.author.discriminator, "nick": message.author.nick, "amount": cleaned[1], "body": cleaned[2]}
-            return dataset
-        except TypeError:
-            raise CommandError("Wartość po poleceniu nie jest liczbą :( .")
+    cleaned[1] = int(cleaned[1])
+    dataset = {"discordid": message.author.id, "name": message.author.name + '#' +
+               message.author.discriminator, "nick": message.author.nick, "amount": cleaned[1], "body": cleaned[2]}
+    return dataset
 
 
 def scores_enumarated_withtitle(title, scorelist):
@@ -32,4 +26,4 @@ def scores_enumarated_withtitle(title, scorelist):
 
 
 def showhelp():
-    return ("FabcoinBot zrozumie te polecenia:\n**$dodaj ilość za_co** \n - doda ci fabcoiny (pojawią się na liście po zaakceptowaniu przez Wojtka / Abdu). \n**$wydaj ilość za_co** \n- zarejestruje wydanie Twoich fabcoinów. \n**$top5** \nwyświetli listę 5 osób z największą ilością fabcoinów. \n**$moje** \n-wyświetli ile masz fabcoinów. \nWażne! Zmiany nie są wprowadzane od razu. Każda transakcja przechodzi weryfikację - co może potrwać do 2 dni. \nPrzykładowa poprawne polecenie dodania:\n$dodaj 5 sprzątanie pracowni ")
+    return ("FabcoinBot zrozumie te polecenia:\n**$dodaj ilość za_co** \n - doda ci fabcoiny (pojawią się na liście po zaakceptowaniu przez Wojtka / Abdu). \n**$wydaj ilość za_co** \n- zarejestruje wydanie Twoich fabcoinów. \n**$top5** \nwyświetli listę 5 osób z największą ilością fabcoinów. \n**$moje** \n-wyświetli ile masz fabcoinów. \nWażne! Zmiany nie są wprowadzane od razu. Każda transakcja przechodzi weryfikację - co może potrwać do 2 dni. \nPrzykładowe poprawne polecenie dodania:\n$dodaj 5 sprzątanie pracowni ")
